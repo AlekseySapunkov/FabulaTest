2d62a7eca911c29a1df2a6368997055c29d1aad3
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2023-04-23 11:14:31 +0300

    Summary

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const modal = document.querySelector('.modal');
-const btn = document.querySelector('button[type="submit"]');
+const submitButtons = document.querySelectorAll('button[type="submit"]');
 const closeButton = document.querySelector('.modal__close-button')
 console.log(modal)
 console.log(closeButton)
-btn.addEventListener('click', function (event) {
+submitButtons.forEach(btn =>btn.addEventListener('click', function (event) {
     event.preventDefault();
     modal.style.display = "block"
-  })
+  }))
 closeButton.addEventListener('click', function (event) {
     event.preventDefault();
     modal.style.display = "none"
