0be41c17fd399e5c39b39bf55284ebbbcb562bff
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2023-05-03 13:04:37 +0300

    Summary

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const modal = document.querySelector('.modal');
 const submitButtons = document.querySelectorAll('button[type="submit"]');
 const closeButton = document.querySelector('.modal__close-button')
+const form = document.querySelector('.form')
 console.log(modal)
 console.log(closeButton)
+function checkValidity(event) {
+  const formNode = event.target.form
+  const isValid = formNode.checkValidity()
+
+  formNode.submitButtons.disabled = !isValid
+}
+
+form.addEventListener('input', checkValidity)
+
 submitButtons.forEach(btn =>btn.addEventListener('click', function (event) {
-    event.preventDefault();
     modal.style.display = "block"
+    
   }))
 closeButton.addEventListener('click', function (event) {
     event.preventDefault();
