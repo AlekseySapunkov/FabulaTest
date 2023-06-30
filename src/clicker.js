d3ea677b198f30e4661039d0c962a17bf5f6eed9
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2023-06-30 21:40:07 +0300

    Summary

--- src/clicker.js
+++ src/clicker.js
@@ -1,12 +1,13 @@
 const rightArrow = document.querySelector('.right-arrow');
 const leftArrow = document.querySelector('.left-arrow');
 const partnerLogo = document.querySelectorAll('.main__partners__logo img')
-const result = Array.from(partnerLogo);let logo = document.querySelector('.logo-wrap img')
+const result = Array.from(partnerLogo);
+let logo = document.querySelector('.logo-wrap img')
 let counter = 1;
 
 partnerLogo[counter].style.opacity = 1;
 const rightTurn = () =>{
-       if(counter >= (partnerLogo.length - 1 )){
+       if(counter > (partnerLogo.length - 1 )){
         counter = 0;
     } else{
         counter = counter + 1
